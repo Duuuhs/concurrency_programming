8d7b60e592f4331d2054baf295a4026e62385603
Duuuhs <1948643024@qq.com>
2019-11-13 23:36:46 +0800

    something learn from 《The Art of Java Concurrency Programming》

---
+++ src/com/duuuhs/concurrent/ConcurrencyTest.java
@@ -0,0 +1,64 @@
+package com.duuuhs.concurrent;
+
+/**
+ *  测试单线程与多线程的不同
+ *  硬件信息：Intel i5-7200u 四核心数
+ *  demo源自：《Java并发编程艺术》
+ */
+public class ConcurrencyTest {
+
+    private static final long count = 1000000000;
+
+    /**
+     * 单线程，用的是psvm的主线程
+     * @throws InterruptedException
+     */
+    public static void serial() throws InterruptedException{
+        long start = System.currentTimeMillis();
+        long a = 0;
+        for (int i=0;i<count;i++){
+            a+=5;
+        }
+        long b = 0;
+        for (int j=0;j<count;j++){
+            b+=5;
+        }
+        System.out.println("serial() execute:" + (System.currentTimeMillis()- start));
+    }
+
+    /**
+     * 多线程，用的是psvm的主线程外加一个自定义的Thread，并用Thread.join()来控制。
+     * @throws InterruptedException
+     */
+    public static void concurrency() throws InterruptedException{
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long a = 0;
+                for (int i=0;i<count;i++){
+                    a+=5;
+                }
+            }
+        });
+        thread.start();
+        long b = 0;
+        for (int j=0;j<count;j++){
+            b+=5;
+        }
+        thread.join();
+        System.out.println("concurrency() execute:" + (System.currentTimeMillis()- start));
+    }
+
+    public static void main (String[] args) throws InterruptedException{
+        concurrency();
+        serial();
+
+        /**
+         * count    serial()  concurrency()
+         * 1000w      23ms        51ms
+         *  1e       196ms       200ms
+         *  10e     1554ms      1902ms
+         */
+    }
+}
